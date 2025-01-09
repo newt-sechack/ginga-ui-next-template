b1ed9394c5e019fb4e6f500ac1ab70c710891ccc
newt <ysti2019.win2020.st@gmail.com>
2025-01-09 21:22:34 +0900

    change: move button page to top

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,18 @@
-export default function Home() {
-  return <div></div>;
+import { Button } from "ginga-ui/core";
+import ThemeClient from "ginga-ui/ai";
+
+export default async function Home() {
+  const themeClient = new ThemeClient({
+    clientType: "openai",
+    apiKey: process.env.OPENAI_API_KEY!,
+  });
+
+  const { CSSCode } = await themeClient.generateTheme("fairy tale");
+
+  return (
+    <div>
+      <style>{CSSCode}</style>
+      <Button>aaas</Button>
+    </div>
+  );
 }
