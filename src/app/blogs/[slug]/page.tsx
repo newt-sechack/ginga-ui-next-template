f4e4e8f60d8ef24ad03b60d6f5311c8d5d969d47
newt <ysti2019.win2020.st@gmail.com>
2025-01-14 08:48:59 +0900

    feat: add article page

---
+++ src/app/blogs/[slug]/page.tsx
@@ -0,0 +1,9 @@
+import { Heading } from "ginga-ui/core";
+
+export default async function ArticlePage() {
+  return (
+    <div>
+      <Heading level="h2">Article Title</Heading>
+    </div>
+  );
+}
